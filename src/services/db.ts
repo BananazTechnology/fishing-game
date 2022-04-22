68404640eae211aa1b68d83b547c4ecd61369fda
jstammer3 <90519929+jstammer3@users.noreply.github.com>
2022-04-21 20:19:07 -0400

    Quick fix

--- src/services/db.ts
+++ src/services/db.ts
@@ -6,13 +6,24 @@ const port = process.env.DB_PORT ? +process.env.DB_PORT : undefined
 
 // eslint-disable-next-line no-unused-vars
 export class KeyDB {
-  static getConnection (): mysql.Connection {
-    return mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: port,
-      user: process.env.DB_USER,
-      password: process.env.DB_PWD,
-      database: process.env.DB_NAME
-    })
+  static getConnection (): mysql.Connection|undefined {
+    console.debug('Attempting DB Connection')
+    try {
+      const conn = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: port,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME
+      })
+
+      conn.on('error', function (err: any) {
+        console.log(err)
+      })
+
+      return conn
+    } catch {
+      console.error('Error Code: UA-SRDADB1')
+    }
   }
 }
